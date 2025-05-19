8e344e65154bb17bbf8f55cec93f2e33bbc9f325
Oleksandr Samoliuk <alexandr.samoliuk@gmail.com>
2025-05-19 19:28:04 +0200

    Update 5_point_stensil_for_Poisson's_Equation.py

--- 5_point_stensil_for_Poisson's_Equation.py
+++ 5_point_stensil_for_Poisson's_Equation.py
@@ -3,17 +3,22 @@ import matplotlib.pyplot as plt
 from scipy.sparse import csr_matrix
 from scipy.sparse.linalg import spsolve
 from scipy.sparse import eye, kron, diags
+from scipy.sparse import find
 from mpl_toolkits import mplot3d
 from mpl_toolkits.mplot3d import Axes3D 
 import sympy as sp
 import inspect
 import re
 
+# -----------------------------------------------------------------------------------------------------------------------------------------
+save_path = '5-point_stensil.pdf'
+# -----------------------------------------------------------------------------------------------------------------------------------------
+
 pi = np.pi
 
 # Grid size
 
-n = 1000
+n = 10
 
 l = 1
 h = 1
@@ -25,7 +30,8 @@ dy = h/(n+1)
 def f(x,y):
     # return np.cos(x*pi+pi/2)*np.sin(y*pi)
     # return 1+0*x
-    return np.exp((x+y**2)*np.sin(30*x)*np.cos(30*y))*x
+    # return np.exp((x+y**2)*np.sin(30*x)*np.cos(30*y))*x
+    return 2*x*y
 
 def function_to_latex(func):
     source_lines = inspect.getsource(func).split('\n')
@@ -39,13 +45,13 @@ def function_to_latex(func):
             expressions.append(match.group(1))
     latex_expressions = []
     for expr in expressions:
-        expr = expr.replace('np.', '')  # Remove 'np.' prefix
+        expr = expr.replace('np.', '')  
         sympy_expr = sp.sympify(expr)
         latex_expr = sp.latex(sympy_expr)
         latex_expressions.append(latex_expr)
     return latex_expressions
 
-f_latex = function_to_latex(f)[-1]  # Last (active) expression
+f_latex = function_to_latex(f)[-1]  
 
 # Matrix construction
 T = 4 * eye(n) - diags([1, 1], [1, -1], shape=(n, n))
@@ -67,33 +73,54 @@ Z_interior = u.reshape(n,n)
 Z_u = np.zeros((n+2, n+2))
 Z_u[1:-1, 1:-1] = Z_interior
 
-# Plot
+# -----------------------------------------------------------------------------------------------------------------------------------------
 
-fig = plt.figure(figsize=(15, 10))
-gs = fig.add_gridspec(1, 3, width_ratios=[1, 1, 1])  # Fixed ratio
-U = [fig.add_subplot(gs[0, i], projection='3d' if i==2 else None) for i in range(3)]
+fig = plt.figure(figsize=(15, 15))
+gs = fig.add_gridspec(2, 2, width_ratios=[0.5, 0.5])
 
-plt.subplots_adjust(wspace=0.4)  
+# Create subplots
+U = [[fig.add_subplot(gs[i, j], projection='3d' if (i, j) == (1, 1) or (i, j) == (0, 1) else None) for j in range(2)] for i in range(2)]
 
-for axis in [U[2].xaxis.pane, U[2].yaxis.pane, U[2].zaxis.pane]:
+plt.subplots_adjust(wspace=0.1, hspace=0.3)
+
+
+U[0][1].grid(False)
+U[0][1].set_xlabel('X', color='#D81159')
+U[0][1].set_ylabel('Y', color='#D81159')
+U[0][1].set_zlabel('$u(x,y)$', color='#D81159')
+U[0][1].plot_surface(X, Y, Z_u, cmap='viridis', linewidth=1)
+U[0][1].set_title(fr"$\frac{{\partial^2 u}}{{\partial x^2}} + \frac{{\partial^2 u}}{{\partial y^2}} = {f_latex}$", fontsize=20)
+
+for axis in [U[0][1].xaxis.pane, U[0][1].yaxis.pane, U[0][1].zaxis.pane]:
     axis.fill = False
     axis.set_edgecolor('w')
-U[2].grid(False)
 
-U[2].set_xlabel('X', color = '#D81159')
-U[2].set_ylabel('Y', color = '#D81159')
-U[2].set_zlabel('u(x,y)', color = '#D81159')
+rows, cols, values = find(A_sparse)
+U[0][0].scatter(rows, cols, c=values, marker = 's', s=300 * U[0][0].get_position().width / n, cmap='viridis')
+U[0][0].set_aspect('equal')
+# U[0][0].spy(A_sparse, markersize=300 * U[0][0].get_position().width / n)
+U[0][0].set_title(f"2D Poisson Equation (n={n})")
 
-U[2].plot_surface(X, Y, Z_u, cmap = 'viridis', linewidth = 1)
-# U[2].plot_surface(X, Y, f(X,Y), cmap = 'inferno', linewidth = 1)
-U[2].set_title(fr"$\frac{{\partial^2 u}}{{\partial x^2}} + \frac{{\partial^2 u}}{{\partial y^2}} = {f_latex}$", 
-               fontsize=20)
 
-U[0].spy(A_sparse, markersize= 300*U[0].get_position().width/(n))
-U[0].set_title(f"2D Poisson Equation (n={n})")
-U[1].imshow(Z_u, cmap='viridis', interpolation='nearest') 
-for i in range(1):
-    U[i].grid(False)
-fig.suptitle("5-point stensil for Poisson's Equation")
+U[1][0].set_title('Heatmap')
+U[1][0].imshow(Z_u, cmap='viridis', interpolation='nearest')
+U[1][0].set_xticks([0,n])
+U[1][0].set_xlabel('X')
+U[1][0].set_yticks([0,n])
+U[1][0].set_ylabel('Y')
+
+U[1][1].grid(False)
+U[1][1].set_title(fr"$f(x,y) = {f_latex}$")
+U[1][1].plot_surface(X, Y, f(X,Y), cmap='inferno', linewidth=1)
+U[1][1].set_xlabel('X', color='#D81159')
+U[1][1].set_ylabel('Y', color='#D81159')
+U[1][1].set_zlabel('$f(x,y)$', color='#D81159')
+
+for axis in [U[1][1].xaxis.pane, U[1][1].yaxis.pane, U[1][1].zaxis.pane]:
+    axis.fill = False
+    axis.set_edgecolor('w')
+
+fig.suptitle("5-point stencil for Poisson's Equation", fontsize = 23)
+fig.savefig(f'{save_path}')
 plt.show()
 
